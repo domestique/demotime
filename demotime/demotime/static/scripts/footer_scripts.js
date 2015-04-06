c4aff6971103cfb729fa0c2b15de9a8913eb15e5
Danny Peck <danny.peck@analytehealth.com>
2015-04-06 10:04:00 -0400

    Some UI love for nested comments.

--- demotime/demotime/static/scripts/footer_scripts.js
+++ demotime/demotime/static/scripts/footer_scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     $('.lightboxed').fancybox();
     $('.content input[type="text"]').first().focus();
+
+    $('.summary a').click(function(event) {
+        event.preventDefault();
+        $(this).parents('.summary').next().slideToggle();
+    });
 });
