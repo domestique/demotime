6c79a4e15897a62c13740b8cf84f62486b7b8e00
Mark Rogers <f4nt@f4ntasmic.com>
2015-04-12 15:14:25 -0500

    resolves #9. Django-Macros installed. Only difference I see is the old had 'usemacro' and this uses 'use_macro'

--- dt/dt/settings.py
+++ dt/dt/settings.py
@@ -30,12 +30,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party Libs
+    'macros',
+    # Our libs
     'demotime',
 )
 
