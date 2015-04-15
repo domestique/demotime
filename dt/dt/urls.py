a508d585963ed01b8aad9c59bbb032e07ec55f41
Mark Rogers <f4nt@f4ntasmic.com>
2015-04-14 23:29:39 -0500

    darn typos

--- dt/dt/urls.py
+++ dt/dt/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     url('', include('demotime.urls')),
 )
 
-if not os.environs.get('DT_PROD'):
+if not os.environ.get('DT_PROD'):
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
