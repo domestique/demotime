3fc7ac2a37823779c154a7b23ca03869ce6c0e42
Mark Rogers <f4nt@f4ntasmic.com>
2016-04-02 14:30:56 -0500

    restructuring migrations

--- demotime/demotime/migrations/0021_messagebundle.py
+++ demotime/demotime/migrations/0021_messagebundle.py
@@ -7,42 +7,6 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-def create_message_bundles(apps, schema_editor):
-    Message = apps.get_model('demotime', 'Message')
-    MessageBundle = apps.get_model('demotime', 'MessageBundle')
-    User = apps.get_model('auth', 'User')
-
-    for user in User.objects.all():
-        for msg in user.receipient.all():
-            if msg.bundle:
-                continue
-
-            if not msg.review:
-                bundle = MessageBundle.objects.create(
-                    review=None,
-                    owner=msg.receipient,
-                )
-                msg.bundle = bundle
-                msg.save(update_fields=['bundle'])
-            else:
-                bundle, _ = MessageBundle.objects.get_or_create(
-                    review=msg.review.review, # its really a ReviewRevision
-                    owner=msg.receipient,
-                )
-                Message.objects.filter(
-                    review=msg.review,
-                    receipient=msg.receipient,
-                ).update(bundle=bundle)
-
-            if all(read for read in bundle.message_set.values_list('read', flat=True)):
-                bundle.read = True
-
-            if all(deleted for deleted in bundle.message_set.values_list('deleted', flat=True)):
-                bundle.deleted = True
-
-            bundle.save(update_fields=['read', 'deleted'])
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -71,5 +35,4 @@ class Migration(migrations.Migration):
             name='bundle',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='demotime.MessageBundle'),
         ),
-        migrations.RunPython(create_message_bundles),
     ]
--- demotime/demotime/migrations/0023_auto_20160401_0153.py
+++ demotime/demotime/migrations/0023_auto_20160401_0153.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('demotime', '0021_messagebundle'),
+        ('demotime', '0022_migrate_bundles'),
     ]
 
     operations = [
