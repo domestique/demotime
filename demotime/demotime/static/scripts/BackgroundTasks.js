d76ce02d2252c56e96cadcfb4d429cc97ebc3368
Danny Peck <danny.peck@analytehealth.com>
2016-04-07 09:22:10 -0400

    Notify users viewing a review if new comments are added while viewing #57

--- demotime/demotime/static/scripts/BackgroundTasks.js
+++ demotime/demotime/static/scripts/BackgroundTasks.js
@@ -42,9 +42,11 @@ DemoTime.BackgroundTasks = Backbone.View.extend({
         });
 
         req.success(function(data) {
+            var update_obj = data.bundles[0].messages[0],
+                bundle_obj = data.bundles[0];
             if (data.message_count > 0 && !self.options.noty) {
                 self.options.noty = noty({
-                    text: 'This review has been updated (click to refresh)',
+                    text: update_obj.message_title + ' (click to refresh)',
                     layout: 'bottomRight',
                     type: 'warning',
                     animation: {
@@ -55,7 +57,26 @@ DemoTime.BackgroundTasks = Backbone.View.extend({
                     },
                     callback: {
                         onCloseClick: function() {
-                            window.location.reload();
+                            // Mark message as read on balloon click
+                            var bundle_array = new Array();
+
+                            // Create an array from the bundle pk
+                            bundle_array.push(bundle_obj.bundle_pk);
+
+                            // Send request to mark message as read
+                            var req = $.ajax({
+                                url: self.options.messages_url,
+                                method: 'POST',
+                                data: {
+                                    'messages': bundle_array,
+                                    'action': 'read'
+                                }
+                            });
+
+                            req.success(function(data) {
+                                // Redirect the user to the comment
+                                window.location.href = window.location.origin + window.location.pathname + '#' + update_obj.comment_pk;
+                            });
                         }
                     }
                 });
