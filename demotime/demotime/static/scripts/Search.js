7c55fd5f05657e701e9ce47666072a45fa0d52ef
Danny Peck <danny.peck@analytehealth.com>
2016-04-09 14:13:41 -0400

    AJAX search refinements. Also making [enter] force a POST search. Table-izing results.

--- demotime/demotime/static/scripts/Search.js
+++ demotime/demotime/static/scripts/Search.js
@@ -20,7 +20,7 @@ DemoTime.Search= Backbone.View.extend({
             self.$el.find('.quickfind-results').slideUp();
         }
 
-        if ($input.val().length > 1 && String.fromCharCode(event.keyCode).match(/[a-zA-Z]/i)) {
+        if ($input.val().length > 1) {
             var reviewer_req = $.ajax({
                 url: self.options.reviewer_url,
                 method: 'POST',
@@ -32,18 +32,13 @@ DemoTime.Search= Backbone.View.extend({
             reviewer_req.success(function(data) {
                 self.reviewers = new SearchModel(data);
 
-                if (self.reviewers.get('reviewers').length) {
-                    // Grab the container template
-                    var html = $('#reviewer_results').html(),
-                        template = _.template(html);
-                    template = template({ user: self.reviewers.get('reviewers') });
+                // Grab the container template
+                var html = $('#reviewer_results').html(),
+                    template = _.template(html);
+                template = template({ user: self.reviewers.get('reviewers') });
 
-
-                    self.$el.find('.quickfind-users').html(template);
-                    self.$el.find('.quickfind-results').slideDown();
-                } else {
-                    self.$el.find('.quickfind-users').html('');
-                }
+                self.$el.find('.quickfind-users').html(template);
+                self.$el.find('.quickfind-results').slideDown();
             });
 
             var review_req = $.ajax({
@@ -56,19 +51,15 @@ DemoTime.Search= Backbone.View.extend({
 
             review_req.success(function(data) {
                 self.reviews = new SearchModel(data);
-                if (self.reviews.get('reviews').length) {
-                    // Grab the container template
-                    var html = $('#review_results').html(),
-                        template = _.template(html);
-                    template = template({ reviews: self.reviews.get('reviews') });
+                // Grab the container template
+                var html = $('#review_results').html(),
+                    template = _.template(html);
+                template = template({ reviews: self.reviews.get('reviews') });
 
-                    self.$el.find('.quickfind-reviews').html(template);
-                    self.$el.find('.quickfind-results').slideDown();
-                } else {
-                    self.$el.find('.quickfind-reviews').html('');
-                }
+                self.$el.find('.quickfind-reviews').html(template);
+                self.$el.find('.quickfind-results').slideDown();
             });
-        } else if (!$input.val().length) {
+        } else {
             this.$el.find('.quickfind-results').slideUp();
         }
     }
