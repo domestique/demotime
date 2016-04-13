c3c310772f1b4a43e4df06b240deee4fa64c7a6a
Danny Peck <danny.peck@analytehealth.com>
2016-04-13 09:10:21 -0400

    If you start to make a comment with attachment, but don't attach anything, an empty file is posted

--- demotime/demotime/static/scripts/footer_scripts.js
+++ demotime/demotime/static/scripts/footer_scripts.js
@@ -44,7 +44,8 @@ var Wysiwyg = new DemoTime.Wysiwyg();
 // Handle review form submits (some light validation on attachments)
 $('.review form').submit(function(e) {
     var form = $(this),
-        proceed = true;
+        proceed = true,
+        reason = '';
 
     // Remove dialog warning
     $(window).unbind('beforeunload');
@@ -52,21 +53,27 @@ $('.review form').submit(function(e) {
 
     $('.attachment-container:visible').each(function() {
         var select = $(this).find('.attachment-type select'),
-            file = $(this).find('.attachment-file input');
+            file = $(this).find('.attachment-file input'),
+            desc = $(this).find('.attachment-desc input');
 
         if (!select.val() && file.val()) {
+            reason = 'an attachment type';
+            proceed = false;
+        }
+        if (!file.val() && desc.val()) {
+            reason = 'an attachment';
             proceed = false;
         }
     });
 
     if (!proceed) {
         e.preventDefault();
-        sweetAlert("Sorry...", "Please select an attachment type.", "error");
+        sweetAlert('Sorry...', 'One of your attachments is missing ' + reason, 'error');
     } else {
         swal ({
-            title: "Updating review",
-            text: "Just a sec...",
-            type: "success",
+            title: 'Updating review',
+            text: 'Just a sec...',
+            type: 'success',
             showConfirmButton: false
         });
     }
