5fb32b20407d3f2b464eeef1e83aad15e51ce4f6
Danny Peck <danny.peck@analytehealth.com>
2016-04-24 10:01:40 -0400

    Typeahead search should wait a moment before executing

--- demotime/demotime/static/scripts/Search.js
+++ demotime/demotime/static/scripts/Search.js
@@ -8,59 +8,66 @@ DemoTime.Search= Backbone.View.extend({
 
     initialize: function(options) {
         this.options = options;
+
+        this.options.interval = 500;
     },
 
     typing: function(event) {
         var $input = $(event.target),
             self = this;
 
+        clearTimeout(self.options.timer);
+        self.options.timer = setTimeout(execute_search, this.options.interval);
+
         if (event.keyCode == 27) {
             $input.val('');
             self.$el.find('.quickfind-results').slideUp();
         }
 
-        if ($input.val().length > 1) {
-            var user_req = $.ajax({
-                url: self.options.user_url,
-                method: 'POST',
-                data: {
-                    action: 'search_users',
-                    name: $input.val()
-                }
-            });
+        function execute_search() {
+            if ($input.val().length > 1) {
+                var user_req = $.ajax({
+                    url: self.options.user_url,
+                    method: 'POST',
+                    data: {
+                        action: 'search_users',
+                        name: $input.val()
+                    }
+                });
 
-            user_req.success(function(data) {
-                self.users = new SearchModel(data);
+                user_req.success(function(data) {
+                    self.users = new SearchModel(data);
 
-                // Grab the container template
-                var html = $('#user_results').html(),
-                    template = _.template(html);
-                template = template({ user: self.users.get('users') });
+                    // Grab the container template
+                    var html = $('#user_results').html(),
+                        template = _.template(html);
+                    template = template({ user: self.users.get('users') });
 
-                self.$el.find('.quickfind-users').html(template);
-                self.$el.find('.quickfind-results').slideDown();
-            });
+                    self.$el.find('.quickfind-users').html(template);
+                    self.$el.find('.quickfind-results').slideDown();
+                });
 
-            var review_req = $.ajax({
-                url: self.options.review_url,
-                method: 'POST',
-                data: {
-                    title: $input.val()
-                }
-            });
+                var review_req = $.ajax({
+                    url: self.options.review_url,
+                    method: 'POST',
+                    data: {
+                        title: $input.val()
+                    }
+                });
 
-            review_req.success(function(data) {
-                self.reviews = new SearchModel(data);
-                // Grab the container template
-                var html = $('#review_results').html(),
-                    template = _.template(html);
-                template = template({ reviews: self.reviews.get('reviews') });
+                review_req.success(function(data) {
+                    self.reviews = new SearchModel(data);
+                    // Grab the container template
+                    var html = $('#review_results').html(),
+                        template = _.template(html);
+                    template = template({ reviews: self.reviews.get('reviews') });
 
-                self.$el.find('.quickfind-reviews').html(template);
-                self.$el.find('.quickfind-results').slideDown();
-            });
-        } else {
-            this.$el.find('.quickfind-results').slideUp();
+                    self.$el.find('.quickfind-reviews').html(template);
+                    self.$el.find('.quickfind-results').slideDown();
+                });
+            } else {
+                this.$el.find('.quickfind-results').slideUp();
+            }
         }
     }
 });
