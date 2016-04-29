8321ca87203bf21cbdece94cc682df0c81e10d0c
Danny Peck <danny.peck@analytehealth.com>
2016-04-29 14:24:25 -0400

    Lowering clock timeout

--- demotime/demotime/static/scripts/Clock.js
+++ demotime/demotime/static/scripts/Clock.js
@@ -33,7 +33,7 @@ var updateTime = function() {
     seconds.animate({transform: "r" + second * 6 + "," + 32 + "," + 32}, 500, mina.elastic);
     count = count + 1;
 
-    if (count > 3600) { // timemout clock after an hour
+    if (count > 600) { // timemout clock after an hour
         $('.clock').css('opacity', '0.5')
             .css({
                 'filter'         : 'blur(1px)',
