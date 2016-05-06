a3c2b0c5493d0d167230781a78e1fb3959c62702
Danny Peck <danny.peck@analytehealth.com>
2016-05-06 10:17:59 -0400

    oh just a fuck-ton of fab UI is all

---
+++ demotime/demotime/static/scripts/Account.js
@@ -0,0 +1,31 @@
+DemoTime.Account = Backbone.View.extend({
+    el: '.manage_user',
+
+    events: {
+        'click .manage_user-item': 'clicked',
+        'click .manage_user-toggler': 'toggle'
+    },
+
+    initialize: function() {
+        this.render();
+    },
+
+    render: function() {
+        var self = this;
+
+        this.$body = this.$el.parents('body');
+
+        this.$body.find('main').click(function() {
+            self.$el.find('ul').slideUp();
+        });
+    },
+
+    toggle: function(event) {
+        event.preventDefault();
+        this.$el.find('ul').slideToggle();
+    },
+
+    clicked: function(event) {
+        this.$el.find('ul').slideUp();
+    }
+});
