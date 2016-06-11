df7f8be2653d0d44e8d1c851dce8369502e70457
Mark Rogers <f4nt@f4ntasmic.com>
2016-06-11 10:21:14 -0500

    typo

--- dt/dt/settings.py
+++ dt/dt/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from ConfigParser import ConfigParser
+from configParser import ConfigParser
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 parser = ConfigParser()
