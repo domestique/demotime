017dd7dee5b43908a42289f760c6cdbbdac891f2
Mark Rogers <f4nt@f4ntasmic.com>
2016-06-11 10:24:50 -0500

    gaaaah python3 is hard

--- dt/dt/settings.py
+++ dt/dt/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from configParser import ConfigParser
+from configparser import ConfigParser
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 parser = ConfigParser()
