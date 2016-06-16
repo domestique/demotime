d26cf14a9e2bd3c6b995cf378affe775290aabc1
Mark Rogers <f4nt@f4ntasmic.com>
2016-06-15 19:39:35 -0500

    now serving static files directly from repo in override

--- dt/startup.sh
+++ dt/startup.sh
@@ -3,4 +3,9 @@
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 uwsgi --ini /home/docker/demotime/configs/uwsgi/dt_uwsgi.ini --daemonize /var/log/uwsgi/dt.log 
+sed -i "s@STATIC_PATH@$STATIC_PATH@g" /etc/nginx/sites-enabled/demotime.conf
+# Symlink in Django admin path
+if [ "$STATIC_PATH" == "/home/docker/demotime/demotime/demotime/static" ]; then
+    ln -s /usr/local/lib/python3.5/dist-packages/django/contrib/admin/static/admin /home/docker/demotime/demotime/demotime/static/admin;
+fi
 nginx -g "daemon off;"
