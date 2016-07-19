afa49e4c5d24edf6efd577b667b53863a9595315
Mark Rogers <f4nt@f4ntasmic.com>
2016-07-18 19:45:22 -0500

    changing default url to local.demotime.com. Introducing a new testing email backend

---
+++ demotime/demotime/email_backends.py
@@ -0,0 +1,24 @@
+import os
+
+from hashlib import md5
+from django.utils.text import slugify
+from django.core.mail.backends.base import BaseEmailBackend
+
+from django.conf import settings
+
+
+class FileOutputEmailBackend(BaseEmailBackend):
+
+    def send_messages(self, email_messages):
+        for message in email_messages:
+            if not os.path.exists(settings.EMAIL_ROOT):
+                os.mkdir(settings.EMAIL_ROOT)
+
+            filename = slugify('{}-{}-{}'.format(
+                message.to,
+                message.subject,
+                md5(message.body.encode('utf-8')).hexdigest(),
+            ))
+            fh = open(os.path.join(settings.EMAIL_ROOT, filename), 'wb')
+            fh.write(message.body.encode('utf-8'))
+            fh.close()
