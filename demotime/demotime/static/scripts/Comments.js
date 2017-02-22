c09f7fb9d9460047f6b82ee78b797c0b27af38ca
Danny Peck <danny.peck@analytehealth.com>
2017-02-22 08:18:50 -0500

    JavaScript should lowercase the profile name when the URL is created

--- demotime/demotime/static/scripts/Comments.js
+++ demotime/demotime/static/scripts/Comments.js
@@ -33,7 +33,12 @@ DemoTime.Comments = Backbone.View.extend({
         // Saving container as an option for global use
         this.options.comment_form_container = button.parents('.comment_form_container');
         this.options.comment = comment_parent.find('.form-control').val();
-        this.options.comment = this.options.comment.replace(/(@)([a-zA-Z0-9_.]+)/g, "<a href=\"/accounts/profile/$2\">$1$2</a>");
+        this.options.comment = this.options.comment,
+                regex = /@([a-z\d_]+)/ig;
+
+        this.options.comment = this.options.comment.replace(regex, function(match) {
+            return "<a href='/accounts/profile/" + match.toLowerCase().replace('@', '') + "'>" + match + "</a>";
+        });
 
         this.options.comment_form_container.find('input, button').prop('disabled', true);
 
